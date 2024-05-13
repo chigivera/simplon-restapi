66fc264cc73b0e0ab63cb554183074bb31ad18d4
chigivera <ayman.benchamkha@gmail.com>
2024-05-13 21:37:33 +0000

    deploying

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 
